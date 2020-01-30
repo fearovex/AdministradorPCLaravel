882b5705d813e29022eda6c2bb65518708e67680
Duvan Gonzalez <programmerloquillo@gmail.com>
2020-01-30 13:18:09 -0500

    Quitando errores que dejo Miguel

--- app/Http/Controllers/RadiusController.php
+++ app/Http/Controllers/RadiusController.php
@@ -5,9 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Config;
-use Collective\Remote\RemoteFacade;
-use Collective\Remote\RemoteServiceProvider;
-use SSH;
 
 class RadiusController extends Controller
 {
