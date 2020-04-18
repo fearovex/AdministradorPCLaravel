889e91167cd6402e80f5245fa42bcfbd2ba4577c
unknown <maacevedo2010@gmail.com>
2020-04-18 12:57:32 -0500

    se corrigen errores en el cms

--- app/Http/Middleware/DatabaseMiddleware.php
+++ app/Http/Middleware/DatabaseMiddleware.php
@@ -36,25 +36,25 @@ class DatabaseMiddleware
             //Habilitar/Instalar la extension de ftp para php
             //Configurar para redireccionar al ftp deseado
 
-            //pre y producción
-            // 'driver' => env('FTP_DRIVER'),
-            // 'host' => env('FTP_HOST'),
-            // 'port' => env('FTP_PORT'),
-            // 'root' => env('FTP_ROOTPATH'),
-            // 'username' => env('FTP_USERNAME'),
-            // 'password' => env('FTP_PASSWORD'),
-            // 'passive' => true,
-            // 'visibility' => 'public',
-            // 'permPublic' => 0777,
-            // 'directoryPerm' => 0777,
+            // pre y producción
+            'driver' => env('FTP_DRIVER'),
+            'host' => env('FTP_HOST'),
+            'port' => env('FTP_PORT'),
+            'root' => env('FTP_ROOTPATH'),
+            'username' => env('FTP_USERNAME'),
+            'password' => env('FTP_PASSWORD'),
+            'passive' => true,
+            'visibility' => 'public',
+            'permPublic' => 0777,
+            'directoryPerm' => 0777,
 
             //local
-            'driver' => 'ftp',
-            'host' => '192.168.0.34',
-            'port' => '21',
-            'root' => '/public/portales',
-            'username' =>'Miguel Acevedo',
-            'password' => 'dogfire1',
+            // 'driver' => 'ftp',
+            // 'host' => '192.168.0.25',
+            // 'port' => '21',
+            // 'root' => '/public/portales',
+            // 'username' =>'Miguel Acevedo',
+            // 'password' => 'dogfire1',
             
             // Optional FTP Settings...
             // 'ssl' => true,
